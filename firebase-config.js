b38b692a21297418937fa791ad7c557b53fbb589
jasim28v-cloud <jasim28v@gmail.com>
2026-03-26 00:40:33 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -16,4 +16,4 @@ const storage = firebase.storage();
 const CLOUD_NAME = 'dk5kas1gc';
 const UPLOAD_PRESET = 'go_kck';
 
-console.log('✅ SHΔDØW System Ready');
+console.log('✅ TikTok Clone Ultra Plus Ready');
