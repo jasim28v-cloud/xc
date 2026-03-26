ca10a8f052097ec469226fa0422d171a43bfd14f
jasim28v-cloud <jasim28v@gmail.com>
2026-03-26 04:18:35 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+// Firebase Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCVyQS6kuOBDyx_FXoGx6xgXxbbzjs5COg",
     authDomain: "fokx-c135a.firebaseapp.com",
@@ -8,12 +9,15 @@ const firebaseConfig = {
     appId: "1:447033620521:web:730fde0a59cb5624128438"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
 const auth = firebase.auth();
 const db = firebase.database();
 const storage = firebase.storage();
 
+// Cloudinary Configuration
 const CLOUD_NAME = 'dk5kas1gc';
 const UPLOAD_PRESET = 'go_kck';
 
-console.log('✅ SHΔDØW Ultra Pro Ready');
+console.log('✅ SHΔDØW System Ready');
