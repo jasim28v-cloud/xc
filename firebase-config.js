3b15e0a3da3d3d88e329f92b7e52ab34388bb29b
jasim28v-cloud <jasim28v@gmail.com>
2026-04-07 00:06:08 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -8,15 +8,11 @@ const firebaseConfig = {
     appId: "1:187788115549:web:0f3c00ff62c1ebc5ed97b4"
 };
 
-// تهيئة Firebase
 firebase.initializeApp(firebaseConfig);
-
-// تعريف المتغيرات العامة التي سيتم استخدامها في كل الملفات
 const auth = firebase.auth();
 const db = firebase.database();
 
-// إعدادات Cloudinary للرفع
 const CLOUD_NAME = 'daemk3hut';
 const UPLOAD_PRESET = 'fok2_k';
 
-console.log('✅ SHΔDØW Firebase Connected Successfully');
+console.log('✅ XClone Ready');
